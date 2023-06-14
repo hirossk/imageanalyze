16f57bf978661ae82c8805fcc848bebdd8e463c5
Yoshi Yoshi <Gakuen@yoshida.com>
2023-06-14 20:48:34 +0900

    円形検出

--- pygui.py
+++ pygui.py
@@ -8,7 +8,7 @@ casceade_path = os.path.join(
     )
 
 cascadeface = cv2.CascadeClassifier("haarcascades\\haarcascade_frontalface_alt.xml")
-cascadeeye = cv2.CascadeClassifier("haarcascades\\haarcascade_eye.xml")
+cascadeeye = cv2.CascadeClassifier("haarcascades\\haarcascade_eye_tree_eyeglasses.xml")
 cascadesmile = cv2.CascadeClassifier("haarcascades\\haarcascade_smile.xml")
 
 def main():
@@ -16,18 +16,14 @@ def main():
     sg.theme('Black')
 
     # efine the window layout
-    layout = [[sg.Text('OpenCV Demo', size=(40, 1), justification='center', font='Helvetica 20')],
+    layout = [[sg.Text('画像解析デモ', size=(40, 1), justification='center', font='Helvetica 20')],
               [sg.Image(filename='', key='image')],
-              [sg.Button('Record',key='Record2', size=(10, 1), font='Helvetica 14'),
-               sg.Button('Edge', size=(10, 1), font='Helvetica 14'),
-               sg.Button('Face', size=(10, 1), font='Helvetica 14'),
-               sg.Button('Eye', size=(10, 1), font='Helvetica 14'),
-               sg.Button('Smile', size=(10, 1), font='Helvetica 14'), ],
-              [sg.Button('Record',key='Record', size=(10, 1), font='Helvetica 14'),
-               sg.Button('Edge', size=(10, 1), font='Helvetica 14'),
-               sg.Button('Stop', size=(10, 1), font='Helvetica 14'),
-               sg.Button('Stop', size=(10, 1), font='Helvetica 14'),
-               sg.Button('Exit', size=(10, 1), font='Helvetica 14'), ]]
+              [sg.Button('撮影開始',key='Record', size=(8, 1), font='Helvetica 14'),
+               sg.Button('Edge', size=(8, 1), font='Helvetica 14'),
+               sg.Button('Face', size=(8, 1), font='Helvetica 14'),
+               sg.Button('Eye', size=(8, 1), font='Helvetica 14'),
+               sg.Button('Circle', size=(8, 1), font='Helvetica 14'),
+               sg.Button('Exit', size=(8, 1), font='Helvetica 14'),  ],]
 
     # ウィンドウの表示
     window = sg.Window('画像処理・認識プログラム',
@@ -35,19 +31,22 @@ def main():
 
     # 入力を待ってループする
     cap = cv2.VideoCapture(0)
+    
     recordingflg = False
     edgeflg = False
     faceflg = False
     eyeflg = False
     smileflg = False
+    circleflg = False
 
     while True:
-        event, values = window.read(timeout=20)
+        event, values = window.read(timeout=100)
 
         if event == 'Exit' or event == sg.WIN_CLOSED:
             return
         elif event == 'Record':
             recordingflg = not recordingflg
+            window['Record'].update(text='撮影中' if recordingflg else '撮影開始',button_color='white on black' if recordingflg else 'black on white')
         elif event == 'Stop':
             recordingflg = False
             img = np.full((1, 1), 0)
@@ -62,12 +61,13 @@ def main():
             eyeflg = not eyeflg
         elif event == 'Smile':
             smileflg = not smileflg
-
+        elif event == 'Circle':
+            circleflg = not circleflg
         if recordingflg:
             ret, frame = cap.read()
             if edgeflg:
                 frame = cv2.Canny(frame,100,200)
-            if faceflg:
+            elif faceflg:
                 face_list = cascadeface.detectMultiScale(frame)
                 if len(face_list) > 0 :
                     #できたとき
@@ -75,7 +75,7 @@ def main():
                         x, y, w, h = face 
                         cv2.rectangle(frame, (int(x),int(y)), (int(x+w), int(y+h)),
                                        (255,255,0), thickness=2) 
-            if eyeflg:
+            elif eyeflg:
                 eye_list = cascadeeye.detectMultiScale(frame)
                 if len(eye_list) > 0 :
                     #できたとき
@@ -91,6 +91,22 @@ def main():
                         x, y, w, h = smile
                         cv2.rectangle(frame, (int(x),int(y)), (int(x+w), int(y+h)),
                                        (128,128,255), thickness=2) 
+                        
+            if circleflg:
+                gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                gray = cv2.medianBlur(gray, 5)
+                
+                
+                circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT,dp=1,minDist=70,
+                                            param1=100,param2=80,minRadius=50,maxRadius=300)
+                
+                if circles is not None:
+                    circles = np.uint16(np.around(circles))
+                    
+                    for i in circles[0,:]:
+                        # draw the outer circle
+                        cv2.circle(frame,(i[0],i[1]),i[2],(0,255,0),2)
+                
             imgbytes = cv2.imencode('.png', frame)[1].tobytes()  # ditto
             window['image'].update(data=imgbytes)
 
