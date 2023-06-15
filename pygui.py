aefbd3032349bbcbb26ed225b8db411853fdb8e9
Hiroyuki Sasaki <sasakidummy@yoshida-g.ac.jp>
2023-06-15 14:12:17 +0900

    スタンドアローンベータ版

--- pygui.py
+++ pygui.py
@@ -1,34 +1,21 @@
 #!/usr/bin/env python
 import PySimpleGUI as sg
-import cv2
 import numpy as np
 import os
+import cv2
+from pyutil import findSquares
+from pyutil import buildwindow
 casceade_path = os.path.join(
         cv2.data.haarcascades, "haarcascades\\haarcascade_frontalface_alt.xml"
     )
-
 cascadeface = cv2.CascadeClassifier("haarcascades\\haarcascade_frontalface_alt.xml")
 cascadeeye = cv2.CascadeClassifier("haarcascades\\haarcascade_eye_tree_eyeglasses.xml")
-cascadesmile = cv2.CascadeClassifier("haarcascades\\haarcascade_smile.xml")
+#フラグのリセット
+
 
 def main():
 
-    sg.theme('Black')
-
-    # efine the window layout
-    layout = [[sg.Text('画像解析デモ', size=(40, 1), justification='center', font='Helvetica 20')],
-              [sg.Image(filename='', key='image')],
-              [sg.Button('撮影開始',key='Record', size=(8, 1), font='Helvetica 14'),
-               sg.Button('Edge', size=(8, 1), font='Helvetica 14'),
-               sg.Button('Face', size=(8, 1), font='Helvetica 14'),
-               sg.Button('Eye', size=(8, 1), font='Helvetica 14'),
-               sg.Button('Circle', size=(8, 1), font='Helvetica 14'),
-               sg.Button('Exit', size=(8, 1), font='Helvetica 14'),  ],]
-
-    # ウィンドウの表示
-    window = sg.Window('画像処理・認識プログラム',
-                       layout, location=(200, 200))
-
+    window = buildwindow()
     # 入力を待ってループする
     cap = cv2.VideoCapture(0)
     
@@ -36,79 +23,90 @@ def main():
     edgeflg = False
     faceflg = False
     eyeflg = False
-    smileflg = False
     circleflg = False
+    squareflg = False
+    gamma = 0
 
     while True:
         event, values = window.read(timeout=100)
 
         if event == 'Exit' or event == sg.WIN_CLOSED:
+            #終了ボタンが押された
             return
         elif event == 'Record':
+            #撮影ボタンが押された
             recordingflg = not recordingflg
-            window['Record'].update(text='撮影中' if recordingflg else '撮影開始',button_color='white on black' if recordingflg else 'black on white')
-        elif event == 'Stop':
-            recordingflg = False
-            img = np.full((1, 1), 0)
-            # this is faster, shorter and needs less includes
-            imgbytes = cv2.imencode('.png', img)[1].tobytes()
-            window['image'].update(data=imgbytes)
-        elif event == 'Edge':
-            edgeflg = not edgeflg
-        elif event == 'Face':
-            faceflg = not faceflg
-        elif event == 'Eye':
-            eyeflg = not eyeflg
-        elif event == 'Smile':
-            smileflg = not smileflg
-        elif event == 'Circle':
-            circleflg = not circleflg
+            window['Record'].update(text='撮影中' if recordingflg else '撮影開始',
+                                    button_color='white on black' if recordingflg else 'black on white')
+            if recordingflg == False:
+                #カメラによる撮影を終了する
+                img = np.full((1, 1), 0)
+                imgbytes = cv2.imencode('.png', img)[1].tobytes()
+                window['image'].update(data=imgbytes)
+                cap.release()
+            else:
+                #カメラによる撮影を開始したとき
+                if cap.isOpened() == False:
+                    cap = cv2.VideoCapture(0)
+
+        # elif event == 'Edge':
+        #     edgeflg = not edgeflg
+        # elif event == 'Face':
+        #     faceflg = not faceflg
+        # elif event == 'Eye':
+        #     eyeflg = not eyeflg
+        # elif event == 'Circle':
+        #     circleflg = not circleflg
+            # if circleflg == False:
+            #     cv2.destroyWindow('circle')
+        # elif event == 'Square':
+        #     squareflg = not squareflg
+            # if squareflg == False:
+            #     cv2.destroyWindow('square')
+        # elif event == 'Slider':
+        #     gamma = int(values['Slider'])
+
         if recordingflg:
             ret, frame = cap.read()
             if edgeflg:
                 frame = cv2.Canny(frame,100,200)
-            elif faceflg:
+            if faceflg:
                 face_list = cascadeface.detectMultiScale(frame)
                 if len(face_list) > 0 :
-                    #できたとき
+                    #顔検出ができたとき
                     for face in face_list :
                         x, y, w, h = face 
                         cv2.rectangle(frame, (int(x),int(y)), (int(x+w), int(y+h)),
                                        (255,255,0), thickness=2) 
-            elif eyeflg:
+            if eyeflg:
                 eye_list = cascadeeye.detectMultiScale(frame)
                 if len(eye_list) > 0 :
-                    #できたとき
+                    #目の検出ができたとき
                     for eye in eye_list :
                         x, y, w, h = eye 
                         cv2.rectangle(frame, (int(x),int(y)), (int(x+w), int(y+h)),
                                        (255,128,255), thickness=2) 
-            if smileflg:
-                smile_list = cascadesmile.detectMultiScale(frame)
-                if len(smile_list) > 0 :
-                    #できたとき
-                    for smile in smile_list :
-                        x, y, w, h = smile
-                        cv2.rectangle(frame, (int(x),int(y)), (int(x+w), int(y+h)),
-                                       (128,128,255), thickness=2) 
                         
             if circleflg:
                 gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
                 gray = cv2.medianBlur(gray, 5)
-                
-                
-                circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT,dp=1,minDist=70,
-                                            param1=100,param2=80,minRadius=50,maxRadius=300)
+                cv2.imshow('circle',gray)
+                circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT,dp=1,minDist=80,
+                                            param1=70,param2=50,minRadius=50,maxRadius=200)
                 
                 if circles is not None:
                     circles = np.uint16(np.around(circles))
-                    
+                    print(circles)
                     for i in circles[0,:]:
                         # draw the outer circle
                         cv2.circle(frame,(i[0],i[1]),i[2],(0,255,0),2)
-                
+            if squareflg:
+                gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                _, bw = cv2.threshold(gray, gamma, 255, cv2.THRESH_BINARY |cv2.THRESH_BINARY_INV)
+                frame = findSquares(bw, frame)
+                cv2.imshow('square',bw)
+
             imgbytes = cv2.imencode('.png', frame)[1].tobytes()  # ditto
             window['image'].update(data=imgbytes)
-
 
 main()
