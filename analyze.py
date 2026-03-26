b7dbc0e9b77599ec13bef60b0940f4011c751406
Hiroyuki Sasaki <hry-sasaki@yoshida-g.ac.jp>
2026-03-26 14:16:38 +0900

    １行増にした。

--- analyze.py
+++ analyze.py
@@ -18,7 +18,8 @@ translate = boto3.client('translate', region_name=REGION)
 layout = [  [title],
             [image],
             #各種ボタンの追加
-            [recordbutton],[exitbutton]
+            [recordbutton],
+            [exitbutton]
             ]
 
 frame = (DIMW,DIMH)
